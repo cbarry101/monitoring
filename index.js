b66549601f4720b90b9a21b901daeb90a2632c04
Connor Barry <cbarry578@gmail.com>
2021-12-16 11:14:29 -0800

    added post endpoint and rollbar error handler

--- index.js
+++ index.js
@@ -10,10 +10,23 @@ const rollbar = new Rollbar({
   captureUnhandledRejections: true,
 })
 
+let students = []
+
+app.use(rollbar.errorHandler())
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
     rollbar.info('HTML file served successfully')
+})
+
+app.post('/api/student', (req,res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+    rollbar.log('Student added successfully', {author: 'Connor', type: 'manual'})
+
+    res.status(200).send(students)
 })
 
 const port = process.env.PORT || 4545
